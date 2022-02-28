3dec7a836f967412331a1bc8c3f8b93ad8ba944c
Lily Su <lsu4@unc.edu>
2022-02-28 16:45:23 -0500

    ugh

---
+++ class1.php
@@ -0,0 +1,36 @@
+<ul class="nav nav-pills">
+    <li class="nav-item"><a class="nav-link active" href="class1.php">INLS 161</a></li>
+    <li class="nav-item"><a class="nav-link" href="class2.php">Class 2</a></li>
+    <li class="nav-item"><a class="nav-link" href="class3.php">Class 3</a></li>
+    <li class="nav-item"><a class="nav-link" href="class4.php">Class 4</a></li>
+    <li class="nav-item"><a class="nav-link" href="class5.php">Class 5</a></li>
+   </ul>
+   <h1>INLS 161</h1>
+   
+   <p>INLS 161 is Tools of Information Literacy.</p>
+   
+   <p>Here is the link to my class's website:</p>
+   
+   
+     <a href="#" class="btn btn-primary btn-sm btn-block" role="button">Task 03 Documents Project (not linked yet)</a>
+     <a href="#" class="btn btn-primary btn-sm btn-block" role="button">Task 04 Data Project (not linked yet)</a>
+     <a href="#" class="btn btn-primary btn-sm btn-block" role="button">Task 05 Presentation Project (not linked yet)</a>
+   
+   
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+   
+   
+    <footer class="footer">
+     <div class="container">
+      <span>Place footer content here.</span>
+     </div>
+      </footer>
+   
+     <script src="js/jquery.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+    </body>
+   </html>
