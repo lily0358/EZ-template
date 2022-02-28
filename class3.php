76f5839f568225316b71488deff2faa2ef0aec98
Lily Su <lsu4@unc.edu>
2022-02-28 16:55:45 -0500

    ah

--- class3.php
+++ class3.php
@@ -1,9 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Lily's Site | INLS 161 </title>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;700&display=swap" rel="stylesheet">
+  <!-- you will need to link to the style sheet to make the CSS work -->
+  <link rel="stylesheet" href="style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+</head>
+  <!-- Start of Navigation code -->
+  <ul class="topnav">
+   <li><a href="index.html">Home</a></li>
+   <li><a href="about.html">About</a></li>
+   <li><a class="active" href="classes.html">Classes</a></li>
+   <li><a href="interests.html">Interests</a></li>
+  </ul>
+   <!-- End of Navigation code -->
+   <div class="container" style="margin-top:30px">
+    <div class="row justify-content-center">
+     <div class="col-sm-10">
+      <div class="card">
+       <div class="card-body">
+   
+   
+   
 <ul class="nav nav-pills">
     <li class="nav-item"><a class="nav-link" href="class1.php">INLS 161</a></li>
-    <li class="nav-item"><a class="nav-link" href="class2.php">Class 2</a></li>
-    <li class="nav-item"><a class="nav-link active" href="class3.php">Class 3</a></li>
-    <li class="nav-item"><a class="nav-link" href="class4.php">Class 4</a></li>
-    <li class="nav-item"><a class="nav-link" href="class5.php">Class 5</a></li>
+    <li class="nav-item"><a class="nav-link" href="class2.php">BUSI 102</a></li>
+    <li class="nav-item"><a class="nav-link active" href="class3.php">CHIN 212</a></li>
+    <li class="nav-item"><a class="nav-link" href="class4.php">ENGL 105</a></li>
+    <li class="nav-item"><a class="nav-link" href="class5.php">MATH 381</a></li>
    </ul>
    <h1>CHIN 212</h1>
    
@@ -11,12 +37,7 @@
    
    <p>Here is the link to my class's website:</p>
    
-   
-     <a href="#" class="btn btn-primary btn-sm btn-block" role="button">Task 03 Documents Project (not linked yet)</a>
-     <a href="#" class="btn btn-primary btn-sm btn-block" role="button">Task 04 Data Project (not linked yet)</a>
-     <a href="#" class="btn btn-primary btn-sm btn-block" role="button">Task 05 Presentation Project (not linked yet)</a>
-   
-   
+
        </div>
       </div>
      </div>
